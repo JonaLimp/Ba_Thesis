ae44eb6e47b55b27e22d5b1051bcb1ba50b5fc74
johnas_duke <crakhad@web.de>
2019-11-07 12:38:09 +0100

    dataloader data_aug changed

--- code/trainer.py
+++ code/trainer.py
@@ -154,7 +154,7 @@ class Trainer(object):
             datagen.fit(self.x_train)
 
             val_datagen = ImageDataGenerator(rescale=1./255)
-            val_datagen.fit(self.test)
+            val_datagen.fit(self.x_val)
 
 
         # help params 
